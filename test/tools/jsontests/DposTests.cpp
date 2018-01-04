e7c9c84068f9207180cc5e376aec65e305982cc3
HuoJunxin <HuoJunxin@fct.com>
2018-01-04 10:16:46 +0800

    将虚拟机测试用例移到另一个文件中，增加测试用例。

--- test/tools/jsontests/DposTests.cpp
+++ test/tools/jsontests/DposTests.cpp
@@ -388,6 +388,13 @@ u256 DposTestClient::balance(const Address& _address) const
 	return block.balance(_address);
 }
 
+bytes DposTestClient::code(const Address& _address) const
+{
+	Block block(m_bc.getInterface(), m_bc.testGenesis().state().db());
+	block.populateFromChain(m_bc.getInterface(), m_bc.getInterface().currentHash());
+	return block.code(_address);
+}
+
 void DposTestClient::sendTransaction(const string & gasLimit, const string & gasPrice, const string & to, const string & value, const string & data, Account& _from)
 {
 	json_spirit::mObject obj;
@@ -404,10 +411,6 @@ void DposTestClient::sendTransaction(const string & gasLimit, const string & gas
 	_from.nonce++;
 }
 
-Address newAddress(const Account& account)
-{
-	return right160(sha3(rlpList(Address(account.address), u256(account.nonce))));
-}
 
 fs::path DposBlockTestSuite::suiteFolder() const
 {
@@ -994,63 +997,6 @@ BOOST_AUTO_TEST_CASE(bcTransactionLimitTest) {}
 
 BOOST_AUTO_TEST_SUITE_END()
 
-string WAVMAddress = "0x0000000000000000000000000000000000000020";
-string ZEROAddress = "0x0000000000000000000000000000000000000000";
-string code1 = "0x0061736d01000000011d0660037f7f7f0060027f7f017f60017f0060027f7f0060000060017e0002a2010b03656e760a5741564d417373657274000303656e76086469765f75323536000003656e760765715f75323536000103656e760767745f75323536000103656e76076c745f75323536000103656e76086d6f645f75323536000003656e76086d756c5f75323536000003656e760b726561644d657373616765000103656e76087375625f75323536000003656e76057465737431000203656e760574657374320003030d0c000000000001010102020405040401700000050301000107d3010d066d656d6f727902000f5f5a706c524b35435432353653315f000b0f5f5a6d69524b35435432353653315f000c0f5f5a6d6c524b35435432353653315f000d0f5f5a6476524b35435432353653315f000e0f5f5a726d524b35435432353653315f000f0f5f5a6c74524b35435432353653315f00100f5f5a6774524b35435432353653315f00110f5f5a6571524b35435432353653315f0012125f5a3966756e6374696f6e313550617261310013125f5a3966756e6374696f6e32355061726132001404696e69740015056170706c7900160ab2040c0a0020012002200010080b0a0020012002200010080b0a0020012002200010060b0a0020012002200010010b0a0020012002200010050b0b002000200110044100470b0b002000200110034100470b0b002000200110024100470b0600200010090b0e0020002802002000280204100a0b0700418a3410090bb60303027f047e017f4100410028020441106b220736020442002104423b2103411021024200210503400240024002400240024020044209560d0020022c00002201419f7f6a41ff017141194b0d01200141a0016a21010c020b420021062004420b580d020c030b200141ea016a41002001414f6a41ff01714105491b21010b2001ad42388642388721060b2006421f83200342ffffffff0f838621060b200241016a2102200442017c2104200620058421052003427b7c2203427a520d000b024020052000520d00200741086a4104100741034b41201000200728020810090b42002104423b210341c00021024200210503400240024002400240024020044209560d0020022c00002201419f7f6a41ff017141194b0d01200141a0016a21010c020b420021062004420b580d020c030b200141ea016a41002001414f6a41ff01714105491b21010b2001ad42388642388721060b2006421f83200342ffffffff0f838621060b200241016a2102200442017c2104200620058421052003427b7c2203427a520d000b024020052000520d0020074108100741074b4120100020072802002007280204100a0b4100200741106a3602040b0b4e040041040b04504000000041100b0b746573745f66756e6331000041200b1e6d6573736167652073686f72746572207468616e206578706563746564000041c0000b0b746573745f66756e633200";
-
-string test_func1_string = "00c01eae1a4067a1";
-string test_func2_string = "00001fae1a4067a1";
-string para1_string = "15030000";
-string para2_string = "672b0000ce560000";
-
-BOOST_FIXTURE_TEST_SUITE(ContractTestsSuite, TestOutputHelperFixture)
-BOOST_AUTO_TEST_CASE(ctCreateContract)
-{
-	DposTestClient client;
-
-	BOOST_REQUIRE(client.get_accounts().size() >= 2);
-	Account& account = client.get_accounts()[0];
-	Account& account2 = client.get_accounts()[1];
-
-	string gasLimit = "0xc350";
-	string gasPrice = "0x04a817c800";
-	string value = "0x0";
-	string data = "";
-
-	//Address m_newAddress = newAddress(account);
-	//string s_address = m_newAddress.hex();
-	client.sendTransaction(gasLimit, gasPrice, WAVMAddress, value, data, account);
-	client.sendTransaction(gasLimit, gasPrice, ZEROAddress, value, data, account2);
-	client.produce_blocks();
-
-	u256 balance = client.balance(Address(account.address));
-	BOOST_REQUIRE(balance < u256(1000000000000000000));
-	u256 cost = u256(1000000000000000000) - balance;
-
-	u256 balance2 = client.balance(Address(account2.address));
-	BOOST_REQUIRE(balance < u256(1000000000000000000));
-	u256 cost2 = u256(1000000000000000000) - balance2;
-
-	BOOST_REQUIRE(cost == cost2);
-	string s_cost = cost.str();
-
-	gasLimit = "0x1f71b2";
-	Address m_newAddress = newAddress(account);
-	string s_address = m_newAddress.hex();
-	client.sendTransaction(gasLimit, gasPrice, WAVMAddress, value, code1, account);
-	client.produce_blocks();
-	u256 balance3 = client.balance(Address(account.address));
-	u256 cost_create = balance - balance3;
-	string s_create = cost_create.str();
-
-	client.sendTransaction(gasLimit, gasPrice, s_address, value, test_func1_string + para1_string, account);
-	client.produce_blocks();
-	u256 balance4 = client.balance(Address(account.address));
-	u256 cost_call = balance3 - balance4;
-	string s_call = cost_call.str();
-
-}
-BOOST_AUTO_TEST_SUITE_END()
 
 }
 }
