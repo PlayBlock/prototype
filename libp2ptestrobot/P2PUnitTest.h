ec111f1e9d97dd0498162052daa91797c0cfa2bf
mamingliang <maminlgiang@fct.com>
2018-03-20 20:44:22 +0800

    添加测试用例 P2PTestInvalidStatusPacket

--- libp2ptestrobot/P2PUnitTest.h
+++ libp2ptestrobot/P2PUnitTest.h
@@ -218,6 +218,31 @@ namespace P2PTest {
 		virtual void step();
 	};
 
+	class P2PTestInvalidStatusPacket : public P2PUnitTest
+	{
+	public:
+		P2PTestInvalidStatusPacket(P2PHostProxy& _proxy) :P2PUnitTest(_proxy) {}
+		~P2PTestInvalidStatusPacket() {}
+
+		//用例名称
+		virtual std::string name() const;
+
+		//用于用例初始化
+		virtual void init();
+
+		//用例销毁
+		virtual void destroy();
+
+		//用来解析传来的协议包
+		virtual void interpret(unsigned _id, RLP const& _r);
+		virtual void interpretProtocolPacket(PacketType _t, RLP const& _r);
+
+		//在host线程
+		virtual void step();
+	private:
+		bool m_passTest;
+	};
+
 	class P2PTestNewBlockHashesAttack : public P2PUnitTest
 	{
 	public:
