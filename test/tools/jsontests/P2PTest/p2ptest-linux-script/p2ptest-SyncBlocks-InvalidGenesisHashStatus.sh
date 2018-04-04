fb3c8c58444f2bf7610e8a151505b921915f1482
zhangshiqi <978663747@qq.com>
2018-04-04 10:24:35 +0800

    增加SyncBlocks状态下的测试用例脚本

---
+++ test/tools/jsontests/P2PTest/p2ptest-linux-script/p2ptest-SyncBlocks-InvalidGenesisHashStatus.sh
@@ -0,0 +1,5 @@
+#
+#P2PUnitTest: P2PTestSyncBlocks   InvalidGenesisHashStatus 
+#
+rm -rf "data0"
+./eth --config "config/config-general.json" -d "data0" -m on -C  -t 1  --sociable -v 4 --p2p-unit-test  P2PTestSyncBlocks  --p2p-sub-test InvalidGenesisHashStatus  --p2pfiller-path "filled-blockchain/ChainASyncB.json"  --chainname "A"
---
+++ test/tools/jsontests/P2PTest/p2ptest-linux-script/p2ptest-SyncBlocks-NewHeaderInvalidHeaderMiddle.sh
@@ -0,0 +1,5 @@
+#
+#P2PUnitTest: P2PTestSyncBlocks   NewHeaderInvalidHeaderMiddle 
+#
+rm -rf "data0"
+./eth --config "config/config-general.json" -d "data0" -m on -C  -t 1  --sociable -v 4 --p2p-unit-test  P2PTestSyncBlocks  --p2p-sub-test NewHeaderInvalidHeaderMiddle  --p2pfiller-path "filled-blockchain/ChainASyncB.json"  --chainname "A"
